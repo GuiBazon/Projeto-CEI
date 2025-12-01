48fcb958d28144ecae94e3aea13daa75ceb1bdd1
GBazon <118981107+GuiBazon@users.noreply.github.com>
2025-12-01 16:12:51 -0300

    Atualizar o server.js (Não testado)

--- sprint3/back/src/server.js
+++ sprint3/back/src/server.js
@@ -1,6 +1,16 @@
 const app = require("./index");
 const PORT = 3000;
 
+const cors = require("cors")
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
+    credentials: true,
+    optionsSucessStatus: 204
+}
+
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
